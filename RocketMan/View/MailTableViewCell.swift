5eaaa0d2fea4e2fd7a86716b6319fb42efd2cc67
Yura Huh <yonseiyhuh@gmail.com>
2020-08-09 15:20:23 +0900

    Second commit

---
+++ RocketMan/View/MailTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MailTableViewCell.swift
+//  RocketMan
+//
+//  Created by 허유라 on 07/08/2020.
+//  Copyright © 2020 Yura. All rights reserved.
+//
+
+import Foundation
---
+++ RocketMan/ViewController/MailListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MailListViewController.swift
+//  RocketMan
+//
+//  Created by 허유라 on 07/08/2020.
+//  Copyright © 2020 Yura. All rights reserved.
+//
+
+import Foundation
